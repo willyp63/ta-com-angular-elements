cfa85900a921b78041457cb65fcb6e3268ab7586
Brisson, Kelsey <kelsey.brisson@transamerica.com>
2018-11-06 16:06:18 -0700

    move pipe class

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { SafePipe } from "@core/pipes/safe.pipe";
+import { SafePipe } from "@shared/pipes/safe.pipe";
 
 /**
  * @ignore
